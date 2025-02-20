c5d4d3ed0f846e2b0f6caa8fc2be74ae55b3ed36
SantiagoIVR <santiago.izquierdo2203@uppuebla.edu.mx>
2025-02-20 15:44:18 -0600

    Rutas

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,12 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\http\Controllers\proveedorcontroller;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('registraproveedor',[proveedorcontroller::class,'registraproveedor'])->name('registraproveedor');
+Route::POST('guardaproveedor',[proveedorcontroller::class,'guardaproveedor'])->name('guardaproveedor');
+Route::get('reporteproveedor',[proveedorcontroller::class,'reporteproveedor'])->name('reporteproveedor');
